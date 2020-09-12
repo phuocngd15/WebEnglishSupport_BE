1bc47cc98a67fd5f19b11a051daec314f5b5ec22
NguyenTranThanhTruc <thanhtrucnguyentran0810@gmail.com>
2020-09-12 13:22:02 +0700

    img & register

---
+++ mongodb/account.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const account = new mongoose.Schema({
+    name: String,
+    age: String,
+    gender:String,
+    address:String,
+    email:String,
+    phone:String,
+    username: String,
+    levels: String,
+    achievement :String
+})
+
+module.exports = mongoose.model("account", account);
---
+++ mongodb/image.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const crudImg = new mongoose.Schema({
+    // title:String,
+    image:String
+})
+
+module.exports =mongoose.model("crudImg", crudImg);
